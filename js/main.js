645d7c82f52588e8675779798f4519e0c9a7e794
Robbie Santos <robbiealixsantos@gmail.com>
2019-10-02 19:42:21 -0700

    add quiz

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {  
+    generateQuestion(0);
+});
+
+var i = 0;
+var correctCount = 0 ;
+
+function generateQuestion(index) {
+    $('input[type=radio]').prop('checked',false);               
+    $("#question").html(quizDataJson[index].question);
+    $("#answerOption1Label").html(quizDataJson[index].option1);
+    $("#answerOption2Label").html(quizDataJson[index].option2);
+    $("#answerOption3Label").html(quizDataJson[index].option3);
+}
+
+function checkAnswer() {
+    var rightAnswer = $("#answerStatus").html("You got it right!");
+
+    if($('#answerOption1').is(':checked') && quizDataJson[i].option1 == quizDataJson[i].answer) { 
+        correctCount++; 
+        rightAnswer;
+    }
+    else if($('#answerOption2').is(':checked') && quizDataJson[i].option2 == quizDataJson[i].answer) {
+        correctCount++;
+        rightAnswer;
+    }
+    else if($('#answerOption3').is(':checked') && quizDataJson[i].option3 == quizDataJson[i].answer) {
+        correctCount++;
+        rightAnswer;
+    }
+    else {
+        $("#answerStatus").html("You got it wrong!");
+    }
+      
+    i++;
+
+    if(quizDataJson.length - 1 < i){
+        $("#feedbackModalLabel").html("You're all done!")
+        $("#answerStatus").html("You finished with " + correctCount + " questions correct out of " + quizDataJson.length);
+        $('#answerStatus').after('<div>Exiting this popup will refresh the page.</div>');
+        $('#feedbackModal').modal('show');
+        $('#feedbackModal').on('hidden.bs.modal', function () {
+            location.reload(true);
+        });
+    }
+
+    $("#currentScore").html("Current Score: " + correctCount + " out of " + quizDataJson.length);
+
+    $('#feedbackModal').modal('show');
+    $('#feedbackModal').on('hidden.bs.modal', function () {
+        generateQuestion(i);
+    });
+}  
---
+++ js/quizData.js
@@ -0,0 +1,72 @@
+var quizDataJson = [
+    {
+        "question" : "Which fictional city does Batman call home?",
+        "option1" : "New Jack City",
+        "option2" : "Gotham City",
+        "option3" : "Springfield",
+        "answer" : "Gotham City"
+    },
+    {
+        "question" : "Traditionally, how many Wonders of the World are there?",
+        "option1" : "3",
+        "option2" : "90",
+        "option3" : "7",
+        "answer" : "7"
+    },
+    {
+        "question" : "Who directed the movie Jaws?",
+        "option1" : "Francis Ford Coppola",
+        "option2" : "George Lucas",
+        "option3" : "Steven Spielberg",
+        "answer" : "Steven Spielberg"
+    },
+    {
+        "question" : "What is a Geiger Counter used to detect?",
+        "option1" : "Radiation",
+        "option2" : "Gold",
+        "option3" : "Water",
+        "answer" : "Radiation"
+    },
+    {
+        "question" : "What is the name of the pirate in Peter Pan?",
+        "option1" : "Captain Morgan",
+        "option2" : "Captain Jack Sparrow",
+        "option3" : "Captain Hook",
+        "answer" : "Captain Hook"
+    },
+    {
+        "question" : "According to the Dr. Seuss book, who stole Christmas?",
+        "option1" : "The Grinch",
+        "option2" : "Jack Skellington",
+        "option3" : "Santa Claus",
+        "answer" : "The Grinch"
+    },
+    {
+        "question" : "Who wrote Julius Caesar, Macbeth and Hamlet?",
+        "option1" : "Mark Twain",
+        "option2" : "Stephenie Meyer",
+        "option3" : "William Shakespeare",
+        "answer" : "William Shakespeare"
+    },
+    {
+        "question" : "Who wasn't a member of the Beatles?",
+        "option1" : "John Lennon",
+        "option2" : "Justin Timberlake",
+        "option3" : "Paul McCartney",
+        "answer" : "Justin Timberlake"
+    },
+    {
+        "question" : "Which of the following is not a type of pasta?",
+        "option1" : "Ziti",
+        "option2" : "Escargot",
+        "option3" : "Spaghetti",
+        "answer" : "Escargot"
+    },
+    {
+        "question" : "Which candy bar shares its name with a galaxy?",
+        "option1" : "Almond Joy",
+        "option2" : "Milky Way",
+        "option3" : "Snickers",
+        "answer" : "Milky Way"
+    }
+];
